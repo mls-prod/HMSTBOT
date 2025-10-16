dd4844f2d92e66d19b824d18b634547b83487b42
MLS PRODUCTION <melisovim@icloud.com>
2025-10-16 15:30:30 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,13 @@ import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth'
 
 //* Add the Web App's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAugjzangT1XjQRASiTAqbF7kuHTE29tzU",
+  authDomain: "maxibot-31995.firebaseapp.com",
+  projectId: "maxibot-31995",
+  storageBucket: "maxibot-31995.firebasestorage.app",
+  messagingSenderId: "167179053543",
+  appId: "1:167179053543:web:b81776cc9a44e7583cb542",
+  measurementId: "G-1RC2YNB90Q"
 };
 
 //* Initialize Firebase
